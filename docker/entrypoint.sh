f7033945d8c2a9f9430c54f06e98e991930c1aa5
Bart <van.ingen.bart@gmail.com>
2025-08-31 11:01:44 +0200

    Update Docker setup and entrypoint script such that tests are found by vscode
    - Set ownership of XDG_RUNTIME_DIR for vscode user
    - Remove redundant PATH export for pytest in entrypoint script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -10,8 +10,5 @@ if [ -d "/ros_ws" ]; then
     echo "Sourced workspace at /ros_ws"
 fi
 
-# Ensure pytest is on PATH
-export PATH="$PATH:/home/dev/.local/bin"
-
 # Execute the command passed into this entrypoint
 exec "$@"
